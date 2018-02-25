0f526f28fb0b25d870619d51beeddc4214dcdd31
antaehyeon <antaehyeon@live.co.kr>
2018-02-25 15:12:33 +0900

    함수 분할 및 코드완성

--- CodeSquard_matter/codeSquard/src/Main.java
+++ CodeSquard_matter/codeSquard/src/Main.java
@@ -5,47 +5,67 @@ import java.util.LinkedHashSet;
 import java.util.Random;
 
 public class Main {
-    public static void main (String[] args) throws Exception {
-        LinkedHashSet<String> lhs = new LinkedHashSet<>();
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String[] numbers;
+
+    public int checkStrike(String[] data, String[] inputData) {
         int strike = 0;
+        for(int i=0; i<3; i++) {
+            if (data[i].equals(inputData[i])) {
+                inputData[i] = "0";
+                strike++;
+            } // if
+        } // for
+        return strike;
+    }
+
+    public int checkBall(LinkedHashSet<String> linkset, String[] inputData) {
         int ball = 0;
-        Random r = new Random();
+        for (int i=0; i<3; i++) {
+            if (inputData[i].equals("0")) continue;
+            if (linkset.contains(inputData[i])) ball++;
+        }
+        return ball;
+    }
+
+    public LinkedHashSet<String> createNumber(LinkedHashSet<String> linkset) {
+        Random rand = new Random();
+        while(true) {
+            if (linkset.size() == 3) break;
+            int a = rand.nextInt(9)+1;
+            linkset.add(String.valueOf(a));
+        } // while
+
+        return linkset;
+    }
+
+    public static void main (String[] args) throws Exception {
+        Main main = new Main();
+        LinkedHashSet<String> linkset = new LinkedHashSet<>();
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputData;
+        int strike = 0, ball = 0;
 
         // 컴퓨터가 3개의 숫자를 중복없이 만드는 부분
         // 생성 후 삽입된 숫자의 순서를 보존하기위해 LinkedHashSet 사용
-        while(true) {
-            if (lhs.size() == 3) break;
-            int a = r.nextInt(9)+1;
-            lhs.add(String.valueOf(a));
-        } // while
+        linkset = main.createNumber(linkset);
 
-        System.out.println(lhs);
+        // Set >> Array
+        String[] data = linkset.toArray(new String[linkset.size()]);
 
         while(true) {
             System.out.print("숫자를 입력해주세요 ex)123 : ");
-            numbers = bf.readLine().split("");
+            inputData = bf.readLine().split("");
 
-            String[] data = lhs.toArray(new String[lhs.size()]);
+            // 스트라이크 체크 부분
+            strike = main.checkStrike(data, inputData);
+            if (strike == 3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                return;
+            } // if
 
-            for(int i=0; i<3; i++) {
-                if (data[i].equals(numbers[i])) {
-                    numbers[i] = "0";
-                    strike++;
-                }
+            // 볼 체크 부분
+            ball = main.checkBall(linkset, inputData);
 
-                if (strike == 3) {
-                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                    return;
-                }
-            } // for
-
-            for (int i=0; i<3; i++) {
-                if (numbers[i].equals("0")) continue;
-                if (lhs.contains(numbers[i])) ball++;
-            }
-
+            // 낫팅 체크 부분
             if (strike == 0 && ball == 0) {
                 System.out.println("NOTHING");
                 continue;
@@ -53,8 +73,7 @@ public class Main {
 
             System.out.println(strike + "스트라이크 " + ball + "볼");
 
-            strike = 0;
-            ball = 0;
+            strike = 0; ball = 0;
         } // while
     }
 }
